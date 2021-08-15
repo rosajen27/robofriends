7ba11f91580886ebdf65806912cecd462e6df081
Jennifer Rosa <rosajen27@gmail.com>
2021-08-15 10:43:09 -0400

    Create Card List

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+            
             <img alt='robot' src={`https://robohash.org/${props.id}?200x200`} />
-
+            
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
             </div>
+
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import 'tachyons';
 import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+
+    <CardList robots={robots} />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
