af2accf37287c92a2dd0f2299399e58e3b1fcf3a
Jennifer Rosa <rosajen27@gmail.com>
2021-08-17 13:16:21 -0400

    JSONPlace Holder API for users + 'Loading...' h1 if long wait time

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        // fetch the users
+        fetch('https://jsonplaceholder.typicode.com/users')
+            // get the response in json
+            .then(response => response.json())
+            // get the users and update the users with setState
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -21,13 +29,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
